a3a0c54aec3903007bf22bcdd616c1cce0c0b403
jenniferlu32 <jenniferlu32@gmail.com>
2021-10-19 21:30:29 -0400

    first

---
+++ employees.js
@@ -0,0 +1,120 @@
+const employees = [
+  { id: 1, name: 'moe'},
+  { id: 2, name: 'larry', managerId: 1},
+  { id: 4, name: 'shep', managerId: 2},
+  { id: 3, name: 'curly', managerId: 1},
+  { id: 5, name: 'groucho', managerId: 3},
+  { id: 6, name: 'harpo', managerId: 5},
+  { id: 8, name: 'shep Jr.', managerId: 4},
+  { id: 99, name: 'lucy', managerId: 1}
+];
+
+const findEmployeeByName = (name, employees) => {
+  let result = {};
+  employees.forEach((employeeObj) => {
+    if (employeeObj.name === name) result = employeeObj;
+  });
+  return result;
+};
+
+//console.log(findEmployeeByName('moe', employees))
+
+const findManagerFor = (employeeObj, employees) => {
+  const managerId = employeeObj.managerId;
+  let result = {};
+  employees.forEach((employeeObj) => {
+    if (employeeObj.id === managerId) result = employeeObj;
+  });
+  return result;
+};
+
+//console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));
+
+const findCoworkersFor = (employeeObj, employees) => {
+  const managerId = employeeObj.managerId;
+  const employeeName = employeeObj.name;
+  let result = [];
+  employees.forEach((employeeObj) => {
+    if (employeeObj.managerId === managerId & employeeObj.name !== employeeName) result.push(employeeObj);
+  });
+  return result;
+};
+
+//console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));
+
+const findManagementChainForEmployee = (employeeObj, employees, managerObj=[]) => {
+  const managerId = employeeObj.managerId;
+  if (managerId) {
+    const currManagerObj = findManagerFor(employeeObj, employees);
+    managerObj.push(currManagerObj);
+    findManagementChainForEmployee(currManagerObj, employees, managerObj);
+  };
+  const result = managerObj.slice().reverse();
+  return result;
+}
+
+//console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));
+//console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));
+/*
+[ { id: 1, name: 'moe' },
+  { id: 2, name: 'larry', managerId: 1 },
+  { id: 4, name: 'shep', managerId: 2 }]
+*/
+
+
+//given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
+console.log(JSON.stringify(generateManagementTree(employees), null, 2));
+/*
+{
+  "id": 1,
+  "name": "moe",
+  "reports": [
+    {
+      "id": 2,
+      "name": "larry",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 4,
+          "name": "shep",
+          "managerId": 2,
+          "reports": [
+            {
+              "id": 8,
+              "name": "shep Jr.",
+              "managerId": 4,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "curly",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 5,
+          "name": "groucho",
+          "managerId": 3,
+          "reports": [
+            {
+              "id": 6,
+              "name": "harpo",
+              "managerId": 5,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 99,
+      "name": "lucy",
+      "managerId": 1,
+      "reports": []
+    }
+  ]
+}
+*/
